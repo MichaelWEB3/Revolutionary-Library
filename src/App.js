2a12a143d0e4efb0d02ae6c2bd154e37d8c60261
Michael da silva ribeiro <55338900+MichaelWEB3@users.noreply.github.com>
2022-02-12 16:33:42 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,16 +1,85 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import './relo.css'
+import { Lista2 } from './list/listTop'
+import { Lista } from './list/listTop'
+
 function App() {
-
-
+  
   const [s, sets] = useState(0)
   const [m, setm] = useState(0)
   const [h, seth] = useState(0)
+  const [ListBOt, setListBot] = useState(null)
+  const [ListTop, setListTop] = useState(null)
+  const [BookSelect, setBookSelect] = useState(null)
+  const [BookSelectBot, setBookSelectBot] = useState(null)
+
+  useEffect(() => {
+    if (Lista2) {
+      setListBot(Lista2)
+    }
+
+    if (Lista) {
+      setListTop(Lista)
+    }
+  }, [])
+
+  function onDragStart1(event, livro, bot = false) {
+    if (!BookSelectBot) {
+      setBookSelect(livro)
+      event
+      .dataTransfer
+      .setData('text/plain', event.target.id);
+
+    event
+      .currentTarget
+      .style
+      .backgroundColor = 'yellow';
+    }
+  }
+
+  function onDragStart2(event, livro) {
+    console.log(livro)
+    if (!BookSelect) {
+      setBookSelectBot(livro)
+
+      event
+        .dataTransfer
+        .setData('text/plain', event.target.id);
+
+      event
+        .currentTarget
+        .style
+        .backgroundColor = 'yellow';
+    }
+
+  }
+
+  function onDragOver1(event) {
+    if (BookSelect) {
+      setListBot([...ListBOt, BookSelect])
+      const newArray = ListTop.filter((item) => item?.id !== BookSelect?.id);
+      setListTop(newArray)
+      setBookSelect(null)
+    }
+    event.preventDefault();
+  }
+  function onDragOver2(event) {
+    if (BookSelectBot) {
+      console.log( BookSelectBot.id)
+      setListTop([...ListTop, BookSelectBot])
+      const newArray = ListBOt?.filter((item) => item?.id !== BookSelectBot?.id);
+      setListBot(newArray)
+      setBookSelectBot(null)
+    }
+    event.preventDefault();
+  }
 
   setInterval(function () {
     refreshClock();
   }, 1000);
+
+
 
   function refreshClock() {
     var time = new Date;
@@ -39,7 +108,7 @@ function App() {
             </div>
             <div class="porca-central2">
             </div>
-          
+
 
             <div class="sec-hand">
               <div class="sec" style={{ transform: s }}>
@@ -55,8 +124,22 @@ function App() {
             </div>
           </div>
         </div>
-        <div className='counter z-10'>
+        <div className='counter z-10 flex flex-col md:p-5 items-start justify-center  '>
+          <div onDragOver={(e) => onDragOver2(e)} className='w-10/12 z-50 top-5 md:h-20  md:m-1  relative md:top-1 flex items-center justify-start' >
+            {ListTop?.map((livro, index) => {
 
+              return <img className='h-10 md:h-full cursor-pointer' key={index} onDragStart={(e) => onDragStart1(e, livro)} src={require(`${livro?.img}`)} />
+            })}
+
+          </div>
+          <div onDragOver={(e) => onDragOver1(e)} className='w-8/12 h-20 pr-7 m-1    z-50 relative md:top-6 flex items-end justify-end'>
+
+            {ListBOt?.map((livro, index) => {
+
+              return <img onDragStart={(e) => onDragStart2(e, livro)} className='h-10  md:h-full cursor-pointer' key={index} src={require(`${livro?.img}`)} />
+            })}
+
+          </div>
         </div>
         <div className='logo z-10'>
 
---
+++ src/list/listTop.js
@@ -0,0 +1,67 @@
+export  const Lista = [
+    {   id:"1",
+        color: "amarelo",
+        height: "3",
+        letter: "a",
+        img: "./assets/book_a.svg"
+    },
+    {
+        id:"2",
+        color: "vermelho",
+        height: "2",
+        letter: "b",
+        img: "./assets/book_b.svg"
+    },
+    {
+        id:"3",
+        color: "abobora",
+        height: "4",
+        letter: "c",
+        img: "./assets/book_c.svg"
+    },
+    {   
+        id:"4",
+        color: "roxo",
+        height: "4",
+        letter: "d",
+        img: "./assets/book_d.svg"
+    },
+    {
+        id:"5",
+        color: "azul",
+        height: "1",
+        letter: "e",
+        img: "./assets/book_e.svg"
+    },
+    {   
+        id:"6",
+        color: "rosa",
+        height: "2",
+        letter: "f",
+        img: "./assets/book_f.svg"
+    }
+]
+
+export  const Lista2 = [
+    {
+        id:"7",
+        color: "Verde",
+        height: "4",
+        letter: "i",
+        img: "./assets/book_i.svg"
+    },
+    {
+        id:"8",
+        color: "azulEscuro",
+        height: "4",
+        letter: "h",
+        img: "./assets/book_h.svg"
+    },
+    {
+        id:"9",
+        color: "rosa",
+        height: "2",
+        letter: "g",
+        img: "./assets/book_g.svg"
+    }
+]
